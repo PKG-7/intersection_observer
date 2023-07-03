45d2792ac9d50aed7b851c0e0e461825836f3c6f
PKG-7 <pavel.k.group@gmail.com>
2023-07-03 23:42:38 +0300

    finished

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('asd')
+console.log('test')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+        //* повтор анимации
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
